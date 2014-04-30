cea5ee1a2e82e3f9d3bdd9e26c06674990edbccc
--global <--global>
2014-04-30 00:10:29 -0400

    Fixinf fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -229,7 +229,7 @@ module.exports = function (grunt) {
     // The following *-min tasks produce minified files in the dist folder
     cssmin: {
       options: {
-        root: '<%= yeoman.app %>'
+        //-- root: '<%= yeoman.app %>'
       }
     },
 
